1ffc50db39a4b1978ca566aee3ed888b2222ff9e
yu-hanyang <2522821183@qq.com>
2025-05-06 22:52:53 +0800

    feat: 实现mode和value的显示

--- Firmware/clock_part/User/main.c
+++ Firmware/clock_part/User/main.c
@@ -6,6 +6,13 @@
 
 int16_t Speed = 0;
 
+typedef struct
+{
+    uint16_t mode;
+    uint16_t value;
+} user_info;
+user_info info = { 0, 0 };
+
 int main(void)
 {
     OLED_Init();
@@ -16,15 +23,19 @@ int main(void)
     while (1)
     {
         OLED_ShowSignedNum(1, 7, Speed, 5);
-        
+        OLED_ShowString(2, 1, "Mode:");
+        OLED_ShowNum(2, 7, info.mode, 1);
+        OLED_ShowString(3, 1, "value:");
+        OLED_ShowNum(3, 8, info.mode, 3);
+
     }
 }
 
- void TIM2_IRQHandler(void)
- {
-         if(TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
-         {
-             Speed = Encoder_Get();
-             TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-         }
- }
+void TIM2_IRQHandler(void)
+{
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
+    {
+        Speed = Encoder_Get();
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+    }
+}
